f074303ff822b009d2aca4bcbc8af52dec6a6fcc
yilmazy17 <yilmazy17@itu.edu.tr>
2021-07-29 08:52:38 +0300

    BlackPage

---
+++ Form1.Designer.cs
@@ -0,0 +1,55 @@
+﻿
+namespace ScreenSaver
+{
+    partial class YeYScreenSaver
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // YeYScreenSaver
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "YeYScreenSaver";
+            this.ShowInTaskbar = false;
+            this.Text = "YeYScreenSaver";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.YeYScreenSaver_KeyDown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.YeYScreenSaver_MouseDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenSaver
+{
+    public partial class YeYScreenSaver : Form
+    {
+        public YeYScreenSaver()
+        {
+            InitializeComponent();
+        }
+
+        private void YeYScreenSaver_KeyDown(object sender, KeyEventArgs e)
+        {
+            Close();
+        }
+
+        private void YeYScreenSaver_MouseDown(object sender, MouseEventArgs e)
+        {
+            Close();
+        }
+    }
+}
